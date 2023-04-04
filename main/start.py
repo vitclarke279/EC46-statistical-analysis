f219498fc5f70c42928807fbda2a8a3e99190897
vitclarke279 <vlopatyuk@icloud.com>
2023-04-04 20:31:50 +0100

    add function to get list of specified values

--- main/start.py
+++ main/start.py
@@ -1,5 +1,10 @@
-from utils.read_data import convert_csv_into_list_of_dicts
+from utils.read_data import convert_csv_into_list_of_dicts, get_list_of_values
 
 csv_file = "csv_files/uk_ec46_tt.csv"
 
-print(convert_csv_into_list_of_dicts(csv=csv_file))
+list_of_dicts = convert_csv_into_list_of_dicts(csv=csv_file)
+
+awg_areacalc_val_list = get_list_of_values(
+    data_list=list_of_dicts, value_key='awg_areacalc_val'
+)
+print(awg_areacalc_val_list)
--- main/utils/read_data.py
+++ main/utils/read_data.py
@@ -2,7 +2,7 @@ from csv import DictReader
 from typing import List
 
 
-def convert_csv_into_list_of_dicts(csv) -> List[dict]:
+def convert_csv_into_list_of_dicts(csv: str) -> List[dict]:
     """
     Reads a CSV file and converts its contents into a list of dictionaries.
     Args:
@@ -15,3 +15,19 @@ def convert_csv_into_list_of_dicts(csv) -> List[dict]:
         list_of_dicts = list(dict_reader)
 
     return list_of_dicts
+
+
+def get_list_of_values(data_list: List[dict], value_key: str) -> list:
+    """
+    Using the given value key, create the list of values from the given list
+    of dictionaries.
+    Args:
+        data_list (List(dict)): The list of dictionaries to parse.
+        value_key (str): The value key used to extract data from given list of
+        dictionaries.
+    Returns:
+        list: A list of values extracted from data_list using key_value.
+    """
+    return [
+        data_dict[value_key] for data_dict in data_list if value_key in data_dict
+    ]
